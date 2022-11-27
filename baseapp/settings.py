51c3d02af56d595ecaadf79cad9a1730638f2e85
samuel aniekan <samuelaniekan680@gmail.com>
2022-11-27 10:11:39 -0500

    modified

--- baseapp/settings.py
+++ baseapp/settings.py
@@ -130,8 +130,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/STATIC_URL = "assets/"
 STATIC_URL = "assets/"
-STATICFILES_DIRS = (os.path.join(BASE_DIR, "assets"),)
-# STATIC_ROOT = "/home/brooazhv/public_html/assets/"
+#STATICFILES_DIRS = (os.path.join(BASE_DIR, "assets"),)
+STATIC_ROOT = "/home/capiwwth/public_html/assets/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
